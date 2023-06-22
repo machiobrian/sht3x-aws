778274701acfed1064e488b838616919891d7360
machiobrian <machiobrian29@gmail.com>
2023-06-22 13:24:59 +0300

    Define Logic

---
+++ include/sensor.h
@@ -0,0 +1,48 @@
+#include <Wire.h>
+#include <Adafruit_SHT31.h>
+#include "SPI.h"
+
+bool enableHeater = false;
+uint8_t loopCount = 0;
+Adafruit_SHT31 sht31 = Adafruit_SHT31(); // Create sensor object
+
+void setup(){
+    Serial.begin(115200);
+    
+    Serial.println("SHT31 t&h test");
+    if (!sht31.begin(0x44)) {
+        Serial.println("Couldn't find SHT31");
+        while(1) delay(1);
+    }
+    Serial.print("Heater Enabled State");
+    if(sht31.isHeaterEnabled()){
+        Serial.println("Enabled");
+    }else{Serial.println("Disabled");}
+}
+
+void loop(){
+    float t = sht31.readTemperature();
+    float h = sht31.readHumidity();
+
+    if(!isnan(t) && !isnan(h)){//if both readings are not NaN
+    // Print the readings
+    Serial.print("Temperature = "); Serial.print(t); Serial.print(" C");
+    Serial.print(" Humidity = "); Serial.print(h); Serial.println("%");
+    }
+    delay(1000);
+
+    // we are going to toggle heater mode every 30 seconds: 
+    // advantage of the "Heater Mode" is that changes are noted to the -3.0*C
+    if(loopCount >= 30){
+    enableHeater = !enableHeater;
+    sht31.heater(enableHeater);
+    Serial.print("Heater Enabled State: ");
+    
+    if(sht31.isHeaterEnabled())
+        Serial.println("Enabled");
+    else
+        Serial.println("Disabled");
+    loopCount = 0;
+    }
+    loopCount++; // increment loop count
+}
